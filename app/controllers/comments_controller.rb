8232f31b2eff6dedf89dd46784200fbcb5bc4f9a
alfonsodumlao <alfonso.dumlao@gmail.com>
2019-04-15 10:12:16 -0400

    ruby upgrade

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,12 @@
+class CommentsController < ApplicationController
+  before_action :ensure_signed_in
+
+  def index
+    @user = User.find(params[:user_id])
+    @post = Post.find(params[:post_id])
+    @comments = Comment.where(user_id: @user.id,  post_id: @post.id)
+    comments = @posts.order("created_at")
+    render json: comments
+  end
+
+end
