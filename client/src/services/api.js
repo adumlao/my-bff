36faee270dba5cbb0c7ccaebbfdf9d03c6ec8d6f
alfonsodumlao <alfonso.dumlao@gmail.com>
2019-04-16 12:27:26 -0400

    secret stuff

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3000'
+  baseURL: ' https://immense-wildwood-61000.herokuapp.com'
 });
 
 const updateToken = (token) => {
